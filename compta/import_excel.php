4403cae40e17996cfc23244d1ba089ab500664c9
Alexandre QUINZIN <alexandre.quinzin@gmail.com>
2025-10-26 22:18:37 +0100

    ajout info de debug

--- compta/import_excel.php
+++ compta/import_excel.php
@@ -14,14 +14,14 @@
     }
     require_once __DIR__ . '/../objets/compta_imports.php';
 
-    echo "import_excel : REQUEST_METHOD";
+    echo "<p><h1>import_excel : REQUEST_METHOD</h1></p>";
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
-        echo "form_num : REQUEST_METHOD";
+        echo "<p><h1>form_num : REQUEST_METHOD</h1></p>";
         if ( isset($_POST) && isset($_POST["form_num"]) && !empty($_POST["form_num"]) ) {
 
             $num_form = intval( $_POST["form_num"] );
-            echo "num_form : $num_form";
+            echo "<p><h1>num_form : $num_form</h1></p>";
             $objimport = new Compta_Imports($objsite->getDB(), true);
 
             if ( $num_form == 0 ) {
--- compta/imports_selection.php
+++ compta/imports_selection.php
@@ -1,50 +1,27 @@
 <?php
-
 require_once __DIR__ . '/../objets/gestion_site.php';
-
 // require __DIR__ . '/../vendor/autoload.php';
-
 if ( !isset($objsite) || !$objsite->IsAsPriv(Site::DROIT_CS) ) {
-
     Header('Location:/');
-
 }
-
 require_once __DIR__ . '/../objets/compta_imports.php';
-
-
 
 $objimport = new Compta_Imports($objsite->getDB(), true);
 
-
-
 echo "<div class=\"imports\">" . PHP_EOL;
-
 echo "\t<div class=\"imports-box\" id=\"imports-box\">" . PHP_EOL;
 
-
-
 $step=0;
-
 if (isset($_POST['step'])) {
-
     $step = $_POST['step'];
-
 }
-
-
 
 echo $objimport->ShowForm($step);
 
 echo "\t<div id=\"imports-message\"></div>" . PHP_EOL;
-
-
-
 echo "\t</div>" . PHP_EOL; // Fermeture imports-box
-
 echo "</div>" . PHP_EOL; // Fermeture imports
 echo "<div>" . PHP_EOL; // Fermeture imports
-echo "import_selection.php" . PHP_EOL; // Fermeture imports
+echo "import_selection.php : $step" . PHP_EOL; // Fermeture imports
 echo "</div>" . PHP_EOL; // Fermeture imports
-
 ?>
